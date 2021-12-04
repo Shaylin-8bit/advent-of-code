343c17e9f3507c4541dfef02a4fb0959ac520c13
Shaylin C <66368978+Shaylin-TRK@users.noreply.github.com>
2021-12-03 20:36:05 -0600

    Update to handle string output

--- parser.c
+++ parser.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-
-void parse(char* input, char* output) {
+void parse(char* input, char* output, char t) {
   FILE* inptr = fopen(input, "r");
   FILE* outptr = fopen(output, "w");
   char c;
-
-  fputs("{ ", outptr);
-
+  // true t val will format as char*, false as int
+  fputs((t ? "char* ctx[] = {\n  \"" : "int* ctx = {\n  "), outptr);
   while ((c = getc(inptr)) != EOF) {
-    c != '\n' ? putc(c, outptr) : fputs(",\n  ", outptr);
-  }
-  
-  fputs("\n}", outptr);
+    c != '\n' ? putc(c, outptr) : fputs((t ? "\",\n  \"" : ",\n  "), outptr);
+  }  
+  fputs((t ? "\"\n};" : "\n};"), outptr);
 
   fclose(inptr);
   fclose(outptr);
